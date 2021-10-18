d4e875aa1b6c11ac1d8679d8e5f8c0404fbc1012
Ezgi Karalı <ezgi.karali505@gmail.com>
2021-10-19 02:57:04 +0300

    my commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+let konutKredileri = ["Konut Kredisi", "Emlak KOnut Kredisi", "Kamu Konut Kredisi", "Özel Konut Kredisi"]
+
+console.log("<ul>")
+for(let i=0; i<konutKredileri.length; i++) {
+    console.log("<li>" + konutKredileri[i] + "</li>")
+ 
+}
+console.log("</ul>")
